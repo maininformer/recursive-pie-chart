6c5409e4b62039d4231ea1bb990ebb52a5326951
plumSemPy <emin@berkeley.edu>
2018-04-10 22:16:47 -0700

    working hook

--- pie.py
+++ pie.py
@@ -1,14 +1,15 @@
+from pdb import set_trace as st
+
 def good_parenting(cls):
-    print('yes')
     _sentinel = object()
     # save the old set attribute method
     old_setattr = getattr(cls, '__setattr__', None)
     def __setattr__(self, name, value):
-        # set the attribute
-        old_setattr(self, name, value)
         # get the old value and update the parents children
         old = getattr(self, name, _sentinel)
-        if old is not _sentinel and name == 'parent' and old !=value and type(value) is Slice:
+        # set the attribute
+        old_setattr(self, name, value)
+        if name == 'parent' and old !=value and type(value) is Slice:
             value.children.append(self)
     cls.__setattr__ = __setattr__
     return cls
@@ -21,5 +22,7 @@ class Slice(object):
         self.children = []
 
 
-draw({'cash':None, 'equity':{'owner':None, 'vc':None}})
+
+
+# draw({'cash':None, 'equity':{'owner':None, 'vc':None}})
 
