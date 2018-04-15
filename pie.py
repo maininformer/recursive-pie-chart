9b2c1b1c85bac1075edcc35a6db42acba30b4429
plumSemPy <emin@berkeley.edu>
2018-04-14 23:01:06 -0700

    working pie with levels and area redistribution

--- pie.py
+++ pie.py
@@ -11,18 +11,57 @@ def auto_register_with_parent(cls):
         old_setattr(self, name, value)
         if name == 'parent' and old !=value and type(value) is Slice:
             value.children.append(self)
+            old_setattr(self, 'level', value.level+1)
+            children = value.children
+            for child in children: # redistribute among the children
+                __setattr__(child, 'area', value.area/len(children))
+        if name == 'area' and old != value:
+            if len(self.children) > 0:  # cannot be on the previous line; will break on the first pass of the constructor
+                children = self.children
+                for child in children:
+                    old_setattr(child, 'area', self.area/len(children))
+
     cls.__setattr__ = __setattr__
+
     return cls
 
 @auto_register_with_parent
 class Slice(object):
-    def __init__(self, label='template', parent=None):
+    def __init__(self, label='template', parent=None, level=0, area=1.0):
         self.parent = parent
-        self.label = label
         self.children = []
+        self.label = label
+        self.level = level
+        self.area = area
 
 
 
 
 # draw({'cash':None, 'equity':{'owner':None, 'vc':None}})
 
+def tests():
+    def test_children_should_register_themselves_to_parents():
+        p=Slice()
+        c=Slice()
+        c.parent=p
+        assert p.children[0] is c
+
+    def test_area_should_redistribute_correctly():
+        p=Slice()
+        c1=Slice()
+        c1.parent=p
+
+        cc1=Slice()
+        cc1.parent=c1
+
+        c2=Slice()
+        c2.parent=p
+
+        assert c2.area == c1.area == 0.5
+        assert cc1.area == 0.5
+
+
+    test_children_should_register_themselves_to_parents()
+    test_area_should_redistribute_correctly()
+
+tests()
